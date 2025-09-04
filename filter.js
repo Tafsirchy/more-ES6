e80f366b43c0b737e687de7529da3c5bad267fc9
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-04 23:25:06 +0600

    filter

--- filter.js
+++ filter.js
@@ -1,8 +1,10 @@
 const numbers = [4, 8, 12, 3, 5, 16, 45, 10];
 
 const greaterThan10 = numbers.filter( x => x > 10);
-
 console.log(greaterThan10);
+
+const evenNumbers = numbers.filter(x => x%2===0);
+console.log(evenNumbers);
 
 const lessThan10 = numbers.filter( x =>{
     const result = x < 10;
