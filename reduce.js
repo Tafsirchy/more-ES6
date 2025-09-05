3a0aaca42c6a823b7da8f54c5e209a438c402d8a
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-05 09:27:39 +0600

    reduce

---
+++ reduce.js
@@ -0,0 +1,11 @@
+const numbers = [4, 5, 6, 7, 12, 15];
+
+let sum = 0;
+
+for (const number of numbers){
+    sum = sum + number
+}
+console.log(sum)
+
+const total = numbers.reduce((acc, curr) => acc + curr);
+console.log(total)
